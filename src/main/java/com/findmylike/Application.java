f541d683c8dd9e208efaff69534be4d8a907086e
sidailenme <sidailen.me@gmail.com>
2020-12-10 02:42:03 +0300

    work at spring

--- src/main/java/com/findmylike/Application.java
+++ src/main/java/com/findmylike/Application.java
@@ -4,10 +4,13 @@ import com.findmylike.config.VkServer;
 import com.vk.api.sdk.exceptions.ApiException;
 import com.vk.api.sdk.exceptions.ClientException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) throws InterruptedException, ApiException, ClientException {
-        VkServer vkServer = new VkServer();
-        vkServer.start();
+        SpringApplication.run(Application.class);
+
     }
 }
